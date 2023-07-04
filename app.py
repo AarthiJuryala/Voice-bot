5c62163b04a2a54ff49666a75d3fd18123f68956
AarthiJuryala <93989962+AarthiJuryala@users.noreply.github.com>
2023-07-04 08:41:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,126 @@
+import os
+import streamlit as st
+import openai
+import pandas as pd
+from langchain.llms import OpenAI
+from PIL import Image
+from audio_recorder_streamlit import audio_recorder
+import speech_recognition as sr
+from tempfile import NamedTemporaryFile
+import soundfile as sf
+import sounddevice as sd
+import numpy as np
+from streamlit_player import st_player
+import base64
+
+from elevenlabs import generate, play, voices
+from elevenlabs.api.error import UnauthenticatedRateLimitError, RateLimitError
+
+# Set OpenAI API key
+
+
+def app():
+    st.sidebar.write("Enter the required keys to get an answer.")
+    api_key = st.sidebar.text_input("Enter OpenAI API key:")
+    elevenlabs_key = st.sidebar.text_input("Enter ElevenLabs key:")
+    # Title and description
+    st.title("BuddyGPT")
+
+    file_ = open("Images/smallbunny.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="bunny gif">',
+        unsafe_allow_html=True,
+    )
+
+    audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=41_000)
+    if audio_bytes:
+        st.audio(audio_bytes, format="audio/wav")
+
+
+        with NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
+            temp_audio_file.write(audio_bytes)
+            temp_audio_file.close()
+
+            # Convert audio to text using speech recognition
+            r = sr.Recognizer()
+            with sr.AudioFile(temp_audio_file.name) as source:
+                audio = r.record(source)
+            text = r.recognize_google(audio)
+
+            st.write("Converted Text:")
+            st.write(text)
+
+        
+        openai.api_key = api_key
+        
+        
+        prompt = f"Question: {text}\nAnswer:"
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt,
+            max_tokens=100,
+            temperature=0.7,
+            n=1,
+            stop=None,
+            timeout=15,
+        )
+        answer = response.choices[0].text.strip().split('\n')[0]
+        st.write("Generated Answer:")
+        st.write(answer)
+
+
+
+
+        # Delete the temporary audio file
+        os.remove(temp_audio_file.name)     
+        
+
+
+        try:
+            audio = generate(text=answer, voice="Rachel", model='eleven_multilingual_v1',
+                            api_key=elevenlabs_key if elevenlabs_key else st.secrets['elevenlabs_key'])
+
+            # Play the audio
+            st.audio(audio, format='audio/wav')
+
+            
+            gif_base64 = base64.b64encode(file_).decode("utf-8")
+            audio_base64 = base64.b64encode(audio).decode("utf-8")
+
+                        
+            # Create an HTML audio element with the embedded GIF source
+            html_audio = f'''
+            <audio controls>
+            <source type="audio/wav" src="data:audio/wav;base64,{audio_base64}">
+            Your browser does not support the audio element.
+            </audio>
+            '''
+
+            # Create an HTML image element with the embedded GIF source
+            html_image = f'''
+            <img src="data:image/gif;base64,{gif_base64}" alt="gif">
+            '''
+
+            # Display the HTML audio element and GIF in Streamlit
+            st.markdown(html_audio + html_image, unsafe_allow_html=True)
+
+
+
+        except UnauthenticatedRateLimitError:
+            e = UnauthenticatedRateLimitError("Unauthenticated Rate Limit Error")
+            st.exception(e)
+
+        except RateLimitError:
+            e = RateLimitError('Rate Limit')
+            st.exception(e)
+
+        
+
+        
+    
+if __name__ == "__main__":
+    app()
